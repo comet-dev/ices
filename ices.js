68b120a533e93168e4307e4320e98f960d1c8943
Moses Grant <mosesgrant3345@gmail.com>
2016-05-03 19:31:04 -0400

    Update ices.js

--- ices.js
+++ ices.js
@@ -1,3 +1,20 @@
-var ices = require('ices');
+var ices = require('.ices');
+ices.contain("Distributable Packages");
 
-ices.send("Distributable Packages");
+ices.prototype.create = function(){
+  var getPackages = this.getPackages;
+  var organize = require('organize');
+  var comet = require('comet');
+  
+  ices(this.getPackages.comet.organize());
+  ices.save = function(icesFile){
+    ices.prompt("Would you like to save your .ices file?");
+    var filename = ".ices";
+    var getfile = document.createElement("a href");
+    getfile.download = filename;
+    getfile.href = window.URL.display
+    getfile.onclick = collapseElement.destroy();
+    getfile.click();
+  }
+}
+
